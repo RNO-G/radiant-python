681a822140493d038c3bed0c639e45e4b1967411
Patrick Allison <dbarawn@gmail.com>
2021-02-05 19:44:38 +0000

    add multiread/multiwrite functions

--- serialcobsdevice.py
+++ serialcobsdevice.py
@@ -5,40 +5,49 @@ from time import sleep
 class SerialCOBSDevice:
 	def __init__(self, port, baudrate):
 		self.dev = serial.Serial(port, baudrate)
-		
-	def read(self, addr):
+
+        # Multiread isn't necessarily supported for all addresses, be careful!
+	def multiread(self, addr, num):
 		tx = bytearray(4)
 		tx[0] = (addr & 0x7F0000)>>16
 		tx[1] = (addr & 0xFF00)>>8
 		tx[2] = addr & 0xFF
-		tx[3] = 3
+		tx[3] = num - 1
 		toWrite = cobs.encode(tx)
 		# print(toWrite)
 		self.dev.write(toWrite)
 		self.dev.write(b'\x00')
-		# expect 7 bytes back + 1 overhead + 1 framing
-		rx = self.dev.read(9)
-		pk = cobs.decode(rx[:8])
-		val = pk[3]
-		val |= (pk[4] << 8)
-		val |= (pk[5] << 16)
-		val |= (pk[6] << 24)
+		# expect num+3 bytes back + 1 overhead + 1 framing
+		rx = self.dev.read(num + 5)
+		pk = cobs.decode(rx[:(num+5-1)])
+		return pk[3:]
+
+	def read(self, addr):
+		pk = self.multiread(addr, 4)
+		val = pk[0]
+		val |= (pk[1] << 8)
+		val |= (pk[2] << 16)
+		val |= (pk[3] << 24)
 		return val
 
-	def write(self, addr, val):
-		tx = bytearray(7)
+	def multiwrite(self, addr, data):
+		tx = bytearray(3)
 		tx[0] = (addr & 0x7F0000)>>16
 		tx[0] |= 0x80
 		tx[1] = (addr & 0xFF00)>>8
 		tx[2] = addr & 0xFF
-		tx[3] = val & 0xFF
-		tx[4] = (val & 0xFF00)>>8
-		tx[5] = (val & 0xFF0000)>>16
-		tx[6] = (val & 0xFF000000)>>24
+		tx.extend(data)
 		self.dev.write(cobs.encode(tx))
 		self.dev.write(b'\x00')
 		# expect 4 bytes back + 1 overhead + 1 framing
 		rx = self.dev.read(6)
 		pk = cobs.decode(rx[:5])
-		val = pk[3]
-		return val
+		return pk[3]
+
+	def write(self, addr, val):
+		tx = bytearray(4)
+		tx[0] = val & 0xFF
+		tx[1] = (val & 0xFF00)>>8
+		tx[2] = (val & 0xFF0000)>>16
+		tx[3] = (val & 0xFF000000)>>24
+		return self.multiwrite(addr, tx)
