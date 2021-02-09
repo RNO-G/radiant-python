b2803b74d697dd131318bbaf8df07826f014f03f
Patrick Allison <dbarawn@gmail.com>
2021-02-09 13:31:29 +0000

    base class for RADIANT calibs

---
+++ radcalib.py
@@ -0,0 +1,25 @@
+# This class is a bit like the SURF calibrations stuff,
+# which needs to be redone to this function so it can use the common LAB4 Controller
+#
+# We need to have (right now):
+# read_vtrimfb fn
+# read_vadjn fn
+# read_vadjp fn
+
+class RadCalib:
+    # do something at init
+    def __init__(self):
+        pass
+    
+    # do nothing
+    def read_vtrimfb(self, dna):
+        return None
+    
+    # do nothing
+    def read_vadjp(self, dna):
+        return None
+    
+    # do nothing
+    def read_vadjn(self, dna):
+        return None
+    
