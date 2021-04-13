b85e828948be5f4a4498e11902ba0a187990ec60
Patrick Allison <dbarawn@gmail.com>
2021-04-13 18:22:56 +0000

    do an initial transfer after enabling to kick the STARTUP in the bootloader

--- examples/enable_spi.py
+++ examples/enable_spi.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+# This program sets up the SPI so that flashrom can use
+# it when we're in the bootloader.
+
+from Adafruit_BBIO.SPI import SPI
 import Adafruit_BBIO.GPIO as GPIO
 
 # SCHEMATIC PINS ARE MIRRORED
 GPIO.setup("P8_26", GPIO.OUT)
 GPIO.output("P8_26", GPIO.LOW)
+
+spi = SPI(0,0)
+spi.mode = 0
+spi.xfer([0xFF,0xFF,0xFF])
+
