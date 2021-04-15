7ddaeae70f2e47e61cf710fbe23790132af406b2
Patrick Allison <dbarawn@gmail.com>
2021-04-15 20:35:54 +0000

    fix typo in flash_image.sh

--- examples/flash_image.sh
+++ examples/flash_image.sh
@@ -12,8 +12,8 @@ IMAGE=/dev/shm/image.bin
 # Linux SPI device. Change this to what's needed!!
 SPIDEV=/dev/spidev0.0
 SPISPEED=16000
-FLASHROM_DEV = "linux_spi:dev=$SPIDEV,spispeed=$SPISPEED"
-FLASHTYPE = "S25FL256S......0"
+FLASHROM_DEV="linux_spi:dev=$SPIDEV,spispeed=$SPISPEED"
+FLASHTYPE="S25FL256S......0"
 LAYOUT="/home/debian/radiant.layout"
 
 flashrom -p "$FLASHROM_DEV" -c "$FLASHTYPE" -w "$IMAGE" -l "$LAYOUT" -i $1 -VVV
