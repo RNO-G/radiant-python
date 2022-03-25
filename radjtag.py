c29309968da319f9698b5bfd4b57ddef41504794
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-03-25 18:27:08 +0000

    fixes

--- radjtag.py
+++ radjtag.py
@@ -1,25 +1,65 @@
 
 class RadJTAG: 
 
-    def __init__(self, dev): 
+    def __init__(self, dev, debug = False): 
 
         self.dev = dev
+        self.dbg = debug
 
     def disable(self): 
         self.dev.write(self.dev.map['BM_CONTROL'], 0) 
    
-    def clock(self, tdi_val, tms_val):
+    def clock(self, tms_val, tdi_val):
+
+        if self.dbg:
+            print('::clock(', tms_val, ',', tdi_val,')')
+
+        rsp =  self.dev.read(self.dev.map['BM_CONTROL'])
+        if self.dbg:
+            print('  Read from BM_Control:', hex(rsp))
+
+ 
         val = 0x200     
 
         if tdi_val: 
             val |= 1 << 18
         if tms_val: 
             val |= 1 << 17
+
+        if self.dbg:
+            print('  Setting BM_Control to ', hex(val))
+
+        #set tdi/tms
         self.dev.write(self.dev.map['BM_CONTROL'], val) 
+
+        # clock rising edge
         val |= 1 << 16 
+
+        if self.dbg:
+            print('Setting BM_Control to ', hex(val))
         self.dev.write(self.dev.map['BM_CONTROL'], val) 
-        rsp =  self.dev.read(self.dev.map['BM_CONTROL']) & (1<<19) != 0 
-        return rsp 
+
+        rsp =  self.dev.read(self.dev.map['BM_CONTROL'])
+        if self.dbg:
+            print('  Read from BM_Control:', hex(rsp))
+
+
+        #falling edge
+        val &= ~(1 << 16)
+        if self.dbg:
+            print('  Setting BM_Control to ', hex(val))
+        self.dev.write(self.dev.map['BM_CONTROL'], val) 
+
+        rsp =  self.dev.read(self.dev.map['BM_CONTROL'])
+        if self.dbg:
+            print('  Read from BM_Control:', hex(rsp))
+
+        ret = (rsp & (1 << 19 )) != 0
+ 
+        if self.dbg:
+            print('->',ret)
+
+        return ret
 
 
     def tlr(self):
@@ -29,13 +69,13 @@ class RadJTAG:
 
     def enumerate(self, disable_after = True): 
         self.tlr()
-        self.clock(0,1)
-        self.clock(1,1)
-        self.clock(0,1)
+        self.clock(0,0)
+        self.clock(1,0)
+        self.clock(0,0)
         idcode = 0
         for i in range(32):
             idcode >>=1 
-            tdobit = self.clock(0,1)
+            tdobit = self.clock(0,0)
             if tdobit: 
                 idcode |= 0x80000000
         self.tlr() 
