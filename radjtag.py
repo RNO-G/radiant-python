c904c2e3da0c20b075133572c7308acded7a04b6
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-03-29 05:02:47 +0000

    slightly saner... still not working

--- radjtag.py
+++ radjtag.py
@@ -9,17 +9,28 @@ class RadJTAG:
     def disable(self): 
         self.dev.write(self.dev.map['BM_CONTROL'], 0) 
    
-    def clock(self, tms_val, tdi_val):
-
-        if self.dbg:
-            print('::clock(', tms_val, ',', tdi_val,')')
-
+    def tdo(self):
         rsp =  self.dev.read(self.dev.map['BM_CONTROL'])
         if self.dbg:
             print('  Read from BM_Control:', hex(rsp))
+        ret = True if (rsp & (1 <<19)) else False 
+        if self.dbg:
+            print('  ->', ret)
+
+
+
+        return ret 
+
+
+
+    def clock(self, tms_val, tdi_val):
+
+        if self.dbg:
+            print('::clock(tms=', tms_val, ', tdi=', tdi_val,')')
+
 
  
-        val = 0x200     
+        val = 1 << 9 # jtag_enable
 
         if tdi_val: 
             val |= 1 << 18
@@ -36,48 +47,31 @@ class RadJTAG:
         val |= 1 << 16 
 
         if self.dbg:
-            print('Setting BM_Control to ', hex(val))
+            print('  Rising edge, setting BM_Control to ', hex(val))
         self.dev.write(self.dev.map['BM_CONTROL'], val) 
-
-        rsp =  self.dev.read(self.dev.map['BM_CONTROL'])
-        if self.dbg:
-            print('  Read from BM_Control:', hex(rsp))
-
 
         #falling edge
         val &= ~(1 << 16)
         if self.dbg:
-            print('  Setting BM_Control to ', hex(val))
+            print('  Falling edge, Setting BM_Control to ', hex(val))
         self.dev.write(self.dev.map['BM_CONTROL'], val) 
-
-        rsp =  self.dev.read(self.dev.map['BM_CONTROL'])
-        if self.dbg:
-            print('  Read from BM_Control:', hex(rsp))
-
-        ret = (rsp & (1 << 19 )) != 0
- 
-        if self.dbg:
-            print('->',ret)
-
-        return ret
 
 
     def tlr(self):
         for i in range(5): 
-            self.clock(1,1) 
+            self.clock(1,0) 
 
 
-    def enumerate(self, disable_after = True): 
+    def enumerate(self): 
         self.tlr()
         self.clock(0,0)
         self.clock(1,0)
         self.clock(0,0)
         idcode = 0
         for i in range(32):
-            idcode >>=1 
-            tdobit = self.clock(0,0)
-            if tdobit: 
-                idcode |= 0x80000000
+            if self.tdo(): 
+                idcode |= (1 << i)
+            self.clock(0,0)
         self.tlr() 
         return idcode 
 
