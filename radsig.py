8668af99213bd63a3879049114869154e5c231a9
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2023-09-07 11:01:28 +0000

    Modfications to the bit patterns to select the signal generator filter bands for the radiant v3.

--- radsig.py
+++ radsig.py
@@ -47,29 +47,31 @@ class RadSig:
     # 3 : 600 MHz+
     # The pulse path doesn't go through the band filter (because I'm dumb, apparently, the 600 MHz+ path would've been fine)
     # so 'band' for it doesn't matter.
+    #
+    # 2023-08-30 Michael Korntheuer:  changed band addressing for RADIANT V3
     def signal(self, pulse=False, band=0):
-        cur = self.dev.read(self.gpioaddr)
-        cur &= 0xCF
+        cur = self.dev.read(self.gpioaddr)  # self.gpioaddr = 0x400058
+        cur &= 0xCF   # = 0b11001111
         if pulse:
             # set bit 4 not bit 5
-            cur |= 0x10
+            cur |= 0x10     # = 0b00010000
         else:
-            # now we *also* select band, so we kill bits 0/2/3
-            cur &= 0b11000010
+            # now we *also* select band, so we kill bits 0/2/3/7
+            cur &= 0b01000010
             # set bit 5 not bit 4
             cur |= 0b00100000
             if band == 0:
-                # CAL_FIL=00: set only bit 3
+                # CALFIL=1000: set only bit 3
                 cur |= 0b00001000
             elif band == 1:
-                # CAL_FIL=01: set bit 3 and 0
-                cur |= 0b00001001
+                # CAL_FIL=1110: set bit 7 and 3 and 0
+                cur |= 0b10001001
             elif band == 2:
-                # CAL_FIL=10: set only bit 2
+                # CAL_FIL=0001: set only bit 2
                 cur |= 0b00000100
             elif band == 3:
-                # CAL_FIL=11: set bit 2 and 0
-                cur |= 0b00000101
+                # CAL_FIL=0111: set bit 7 and 2 and 0
+                cur |= 0b10000101
             else:
                 self.logger.error("Illegal band, must be 0/1/2/3")
                 return
